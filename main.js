68f91ed3bdf665aacc7ea2d78d3d4019cf4421ca
yrevtovich <yrevtovich@mail.ru>
2020-03-16 00:20:30 +0300

    feat: add display-hide function

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
 const MENU = document.querySelector('.main-navigation');
+const PHONES = document.querySelector('.phones');
+const GALLERY = document.querySelector('.portfolio-gallery');
 
 MENU.addEventListener('click', (event) => {
     MENU.querySelectorAll('a').forEach( (elem) => elem.classList.remove('active'));
     if (event.target.tagName === "A") {
         event.target.classList.add('active');
     }
-})
+});
 
-
-const DISPLAY
-
-const GALLERY = document.querySelector('.portfolio-gallery');
+PHONES.addEventListener('click', (event) => {
+    let phoneClass = event.target.classList[0];
+    PHONES.querySelectorAll('.display').forEach( elem => {
+        if (elem.classList.contains(phoneClass)) {
+            elem.classList.toggle('hide-display');
+        }
+    });
+});
 
 GALLERY.addEventListener('click', (event) => {
     GALLERY.querySelectorAll('IMG').forEach( elem => elem.classList.remove('portfolio-gallery-item-active'));
@@ -19,5 +25,5 @@ GALLERY.addEventListener('click', (event) => {
     if (event.target.tagName === "LI" || event.target.tagName === "IMG") {
         event.target.classList.add('portfolio-gallery-item-active');
     }
-})
+});
 
